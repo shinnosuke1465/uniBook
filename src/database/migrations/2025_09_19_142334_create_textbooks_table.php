e987e04b3f403a3e618fb2b0dafbb20353b2c594
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-09-19 23:36:56 +0900

    textbooksマイグレーションファイル作成

---
+++ src/database/migrations/2025_09_19_142334_create_textbooks_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const TABLE_NAME = 'textbooks';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE_NAME, function (Blueprint $table) {
+                $table->string('id', 36)->primary()->comment('教科書ID');
+                $table->string('name', 255)->comment('教科書名');
+                $table->text('description')->nullable()->comment('説明');
+                $table->unsignedInteger('price')->comment('価格');
+                $table->enum('condition_type', [
+                    'new',           // 新品、未使用
+                    'near_new',      // 未使用に近い
+                    'no_damage',     // 目立った傷や汚れなし
+                    'slight_damage', // やや傷や汚れあり
+                    'damage',        // 傷や汚れあり
+                    'poor_condition' // 全体的に状態が悪い
+                ])->comment('状態');
+                $table->string('university_id')->comment('大学ID');
+                $table->string('faculty_id', 36)->comment('学部ID');
+
+                $table->timestamps();
+                $table->softDeletes();
+
+                // 外部キー制約
+                $table->foreign('university_id')
+                    ->references('id')
+                    ->on('universities');
+                $table->foreign('faculty_id')
+                    ->references('id')
+                    ->on('faculties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+};
