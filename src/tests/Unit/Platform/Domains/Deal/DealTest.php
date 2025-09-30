8e48ac71ecffb54a2a229243344d9aea79173941
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-09-30 12:44:30 +0900

    dealのupdate作成

--- src/tests/Unit/Platform/Domains/Deal/DealTest.php
+++ src/tests/Unit/Platform/Domains/Deal/DealTest.php
@@ -62,4 +62,33 @@ class DealTest extends TestCase
         $this->assertEquals($expectTextbookId, $actualDeal->textbookId);
         $this->assertEquals($expectDealStatus, $actualDeal->dealStatus);
     }
+
+    public function test_updateできること(): void
+    {
+        // given
+        $seller = new Seller(new UserId('11111111-1111-1111-1111-111111111111'));
+        $buyer1 = new Buyer(new UserId('22222222-2222-2222-2222-222222222222'));
+        $textbookId = new TextbookId('33333333-3333-3333-3333-333333333333');
+        $dealStatus1 = DealStatus::create('Listing');
+        $deal = new Deal(
+            new DealId('44444444-4444-4444-4444-444444444444'),
+            $seller,
+            $buyer1,
+            $textbookId,
+            $dealStatus1
+        );
+
+        // when
+        $buyer2 = new Buyer(new UserId('55555555-5555-5555-5555-555555555555'));
+        $dealStatus2 = DealStatus::create('Purchased');
+        $updatedDeal = $deal->update($buyer2, $dealStatus2);
+
+        // then
+        $this->assertNotSame($deal, $updatedDeal);
+        $this->assertEquals($deal->id, $updatedDeal->id);
+        $this->assertEquals($deal->seller, $updatedDeal->seller);
+        $this->assertEquals($deal->textbookId, $updatedDeal->textbookId);
+        $this->assertEquals($buyer2, $updatedDeal->buyer);
+        $this->assertEquals($dealStatus2, $updatedDeal->dealStatus);
+    }
 }
