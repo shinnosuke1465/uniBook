5f035fd3814e3695faef8cb7274a971c76b0e8c2
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-10-06 03:23:55 +0900

    デザイン修正

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -25,7 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
